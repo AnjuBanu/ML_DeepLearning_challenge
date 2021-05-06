b8734d9e58e7e4be436cf42e00f9e787443aa07b
anjum.banu@gmail.com <anjum.banu@gmail.com>
2021-05-06 13:04:28 +0100

    Day 1 ML challenge

--- ML_DL_Challenge/Day_1/linear_regression.py
+++ ML_DL_Challenge/Day_1/linear_regression.py
@@ -6,14 +6,14 @@ from sklearn import datasets
 # Implementation of Linear Regression
 class LinearRegression:
     # Identifying the Data required and its dimensions
-    def fit (self, X, y, intercept = False):
-        if intercept == False:
-            ones = np.ones(len(X)).reshape(len(X) ,1)
-            X = np.concatenate((ones ,X), axis=1)
+    def fit(self, X, y, intercept=False):
+        if intercept==False:
+            ones = np.ones(len(X)).reshape(len(X), 1)
+            X = np.concatenate((ones, X), axis=1)
         self.X = np.array(X)
         self.y = np.array(y)
-        self.N ,self.D = X.shape
-        # Estimate the parame
+        self.N, self.D = X.shape
+        # Estimate the parameters
         XtX = np.dot(self.X.T, self.X)
         XtXin = np.linalg.inv(XtX)
         xty = np.dot(self.X.T, self.y)
@@ -21,22 +21,21 @@ class LinearRegression:
         # Predict the values
         self.yhat = np.dot(self.X, self.beta_hat)
         # Calculate the loss function
-        self.l = (0.5) * np.sum((self.y - self.yhat )* *2)
+        self.l = (0.5) * np.sum((self.y - self.yhat) ** 2)
     # To create predictions
-    def predict (self, x_test, intercept = True):
+    def predict(self, x_test, intercept=True):
         self.test_yhat = np.dot(x_test, self.beta_hat)
 
 # Importing the dataset boston
 boston = datasets.load_boston()
-X = boston['data']                  # Independent features
-y = boston['target']                # Dependent features
-model = LinearRegression()          # Instantiating the model
-model.fit(X,y)                      # Fitting the model
+X = boston['data']                      # Independent features
+y = boston['target']                    # Dependent features
+model = LinearRegression()              # Instantiating the model
+model.fit(X, y)                         # Fitting the model
 
-
-fig, ax = plt.subplots()            # Plotting the actual and predicted values
-sns.scatterplot(y ,model.yhat)
-ax.set_xlabel("y", size = 25)
-ax.set_ylabel(r"$\hat{y}$", size = 25, rotation = 0, labelpad = 25)
-ax.set_title("$y$ vs $\hat{y}$",  size = 25)
+fig, ax = plt.subplots()                # Plotting the actual and predicted values
+sns.scatterplot(y, model.yhat)
+ax.set_xlabel("y", size=25)
+ax.set_ylabel(r"$\hat{y}$", size=25, rotation=0, labelpad=25)
+ax.set_title("$y$ vs $\hat{y}$", size=25)
 sns.despine()
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-#Machine learning and Deep Learning
+![img.png](img.png)
+
+##About
+Python implementations of some of the fundamental Machine Learning models and algorithms from scratch.
+
+The purpose of this project is not to produce as optimized and computationally efficient algorithms as possible but rather to present the inner workings of them in a transparent and accessible way.
+
+---
+### Day1
+
+Ordinary Linear Regression:
+
+Linear Regression is a relatively simple method that is extremely widely-used. It is also a great stepping stone for more sophisticated methods, making it a natural algorithm to study first. A regression analysis is simply a method of estimating the relationship between a dependent variable and a set of independent variables.
+
+Have understood and implemented the Linera regression by using a concept of minimizing the loss and maximizing the likelihood.
+
+![img_1.png](img_1.png)
